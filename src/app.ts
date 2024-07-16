7bbb5c8fed789d5c2775374471380a632536108e
KennethMinn <minthukha2472005@gmail.com>
2024-07-16 20:32:04 +0630

    feat : prisma init

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import http from "http";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+
+const app = express();
+dotenv.config(); //to use env
+const server = http.createServer(app);
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
+
+app.use(express.static("public")); //for file upload
+app.use(morgan("dev")); //http middleware
+app.use(express.urlencoded({ extended: false }));
+
+// Middleware for parsing JSON bodies
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+server.listen(8080, () => {
+  console.log("server connected");
+});
